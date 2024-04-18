5ffcaacba8f71bbb5b9f17d1e18d7f22f757375b
williameom_ <williameom@williameom.xyz>
2024-04-18 20:03:41 +0900

    24.04.18 Fin.

---
+++ study_9/js/index.js
@@ -0,0 +1,26 @@
+const updateClock = () => {
+    const dateReadable = ["주일", "월", "화", "수", "목", "금", "토"];
+    var now = new Date();
+
+    var yr = now.getFullYear();
+    var mo = now.getMonth() + 1;
+    var dy = now.getDate();
+    var dTe= dateReadable[now.getDay()];
+    var hr = now.getHours();
+    var min = now.getMinutes();
+    var sec = now.getSeconds();
+
+    //var hr_ = (hr < 10) ? '0' + hr : hr;
+    var min_ = (min < 10) ? '0' + min : min;
+    var sec_ = (sec < 10) ? '0' + sec : sec;
+
+    if (hr >= 12) {
+        apm = "오후";
+        hr_ = hr - 12;
+    }
+
+    var clock = document.getElementById('clock');
+    clock.textContent = yr + "년 " + mo + "월 " + dy + "일 (" + dTe + ")\n\n"+ apm + " "+ hr_ + ':' + min_ + ':' + sec_;
+};
+
+setInterval(updateClock, 100);
