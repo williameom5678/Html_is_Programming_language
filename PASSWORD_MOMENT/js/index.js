d944d3f7873aa65e631b4b4a7b0f7015a43f7a3e
williameom_ <williameom@williameom.xyz>
2024-08-29 20:13:59 +0900

    24.08.09 FIN

--- PASSWORD_MOMENT/js/index.js
+++ PASSWORD_MOMENT/js/index.js
@@ -1,1 +1,10 @@
 // プロジェクトセカイ カラフルステージ！ feat.初音ミク
+
+// let yamero = function() {
+//     for(;;) {
+//         alert("インターネットをやめる！！！！");
+//     }
+// };
+
+// やめる!!!!
+// document.onload(yamero());
