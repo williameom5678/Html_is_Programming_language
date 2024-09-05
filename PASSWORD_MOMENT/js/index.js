6b594c0121f02caa9a5aeb5cafb3f60ce448477c
williameom_ <williameom@williameom.xyz>
2024-09-05 19:58:56 +0900

    new file:   INPUT_MOMENT/css/index.css 	new file:   INPUT_MOMENT/index.html 	new file:   INPUT_MOMENT/js/index.js 	modified:   PASSWORD_MOMENT/css/index.css 	modified:   PASSWORD_MOMENT/index.html 	modified:   PASSWORD_MOMENT/js/index.js

--- PASSWORD_MOMENT/js/index.js
+++ PASSWORD_MOMENT/js/index.js
@@ -8,3 +8,9 @@
 
 // やめる!!!!
 // document.onload(yamero());
+
+function brazil() {
+    // location.replace("https://lrl.kr/loZe");
+    // window.open("https://lrl.kr/xZqq");
+    window.open("https://www.youtube.com/watch?v=vQEubVwz7Zg&t=10s");
+}
