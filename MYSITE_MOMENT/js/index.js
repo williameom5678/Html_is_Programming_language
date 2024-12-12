783d1ea4c85c0dbfe05bde6d5538d0596a62fa4d
williameom_ <williameom@williameom.xyz>
2024-12-12 19:12:19 +0900

    1212

--- MYSITE_MOMENT/js/index.js
+++ MYSITE_MOMENT/js/index.js
@@ -81,8 +81,8 @@
   };
   // 스크롤 이벤트 연결하기
   const scollMoveEl = document.querySelectorAll("[data-animation-scroll='true']"); 
-  for(let i = 0; i < scollMoveEl.length; i++){
-    scollMoveEl[i].addEventListener('click', function(e){
+  for(let c = 0; c < scollMoveEl.length; c++){
+    scollMoveEl[c].addEventListener('click', function(e){
       const target = this.dataset.target;
       animationMove(target);
     });
